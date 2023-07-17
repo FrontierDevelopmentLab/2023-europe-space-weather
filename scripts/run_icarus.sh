01aaaad363205330b3c542358416f7b31470cd72
Emma Stevenson <emma.stevenson@upm.es>
2023-07-17 12:15:37 +0000

    add icarus run script

---
+++ scripts/run_icarus.sh
@@ -0,0 +1,8 @@
+# Training from scratch
+# init workspace
+
+# convert data (pre-training with center crop)
+python -m s4pi.maps.prep.prep_hao
+# full training
+python -m sunerf.sunerf --wandb_name "hao" --data_path "/mnt/ground-data/prep_HAO/*.fits" --path_to_save "/mnt/training/HAO_v1" --train "config/train.yaml" --hyperparameters "config/hyperparams_icarus.yaml"
+
