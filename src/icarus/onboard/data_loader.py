ecab4290db5089ce8a8d3d745ee26b2f8ac3898b
Chia-Man Hung <chiaman.hung1@gmail.com>
2023-07-17 12:17:20 +0000

    Update.

--- src/icarus/onboard/data_loader.py
+++ src/icarus/onboard/data_loader.py
@@ -36,6 +36,10 @@ class FitsDataModule(LightningDataModule):
             "Data Directory Cor2", os.path.join(os.getcwd(), "Data", "Cor2")
         )
 
+        self.p_train = hparams.get("train percentage", 0.85)
+        self.p_test = hparams.get("test percentage", 0.1)
+        self.p_val = hparams.get("validation percentage", 0.05)
+
     # def prepare_data(self):
     #     pass
 
@@ -44,12 +48,24 @@ class FitsDataModule(LightningDataModule):
         cor1_data = glob.glob("{}/*.fts".format(self.cor1_data_dir))  # inner corona
         cor2_data = glob.glob("{}/*.fts".format(self.cor2_data_dir))  # outer corona
 
-        total_files = len(cor1_data + cor2_data)
+        n_total_files = len(cor1_data + cor2_data)
+        # Should we set proportions?
+        p_train = self.p_train
+        p_test = self.p_test
+        p_val = self.p_val
+
+        n_train = int(n_total_files * p_train)
+        n_test = int(n_total_files * p_test)
+        n_val = int(n_total_files * p_val)
+
+        # There is a bit of time correlation here
+        # - frankly, not important as long as timestamps are used to handle files, ie
+        # can correlate timesteps to files, allowing us to reconstruct the correct physics
 
         self.fits_test = -1
         self.fits_predict = -1
         fits_full = -1
-        self.fits_train, self.fits_val = random_split(fits_full, [55000, 5000])
+        self.fits_train, self.fits_val = random_split(fits_full, [n_train, n_val])
 
     def train_dataloader(self):
         return DataLoader(self.fits_train, batch_size=self.batch_size)
