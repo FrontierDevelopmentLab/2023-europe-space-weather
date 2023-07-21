separate CME evaluation, remove scaling quick fix

sunerf/evaluation/loader.py
@@ -28,7 +28,7 @@ class SuNeRFLoader:
         self.end_time = state['end_time']
         self.config = state['config']
 
-        encoder = PositionalEncoder(**state['encoder_kwargs'], scale_factor=340) # TODO DON'T HARD CODE Yy
+        encoder = PositionalEncoder(**state['encoder_kwargs'])
         self.encoding_fn = lambda x: encoder(x)
         self.coarse_model = nn.DataParallel(state['coarse_model']).to(device)
         self.fine_model = nn.DataParallel(state['fine_model']).to(device)

sunerf/train/model.py
@@ -94,7 +94,7 @@ class PositionalEncoder(nn.Module):
     Sine-cosine positional encoder for input points.
     """
 
-    def __init__(self, d_input: int, n_freqs: int, scale_factor: float = 2., log_space: bool = False):
+    def __init__(self, d_input: int, n_freqs: int, scale_factor: float = 340., log_space: bool = False):
         """
 
         Parameters