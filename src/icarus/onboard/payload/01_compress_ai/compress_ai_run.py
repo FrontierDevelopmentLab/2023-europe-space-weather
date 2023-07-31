41287b80aa4f0794261b6e7ff5cf613852e019ba
Chia-Man Hung <chiaman.hung1@gmail.com>
2023-07-31 09:45:27 +0000

    Ran pre-commits and exclude src/icarus/data.

--- src/icarus/onboard/payload/01_compress_ai/compress_ai_run.py
+++ src/icarus/onboard/payload/01_compress_ai/compress_ai_run.py
@@ -1,43 +1,50 @@
+import math
+import os
+
 import matplotlib.pyplot as plt
-import math, os, torch
-from torchvision import transforms
 import numpy as np
-from pytorch_msssim import ms_ssim
-from compressai.zoo import bmshj2018_factorized # “Variational Image Compression with a Scale Hyperprior”
+import torch
 from astropy.io import fits
+from compressai.zoo import (  # “Variational Image Compression with a Scale Hyperprior”
+    bmshj2018_factorized,
+)
+from pytorch_msssim import ms_ssim
+from torchvision import transforms
 
 # change name to 2023-europe-space-weather/src/icarus/data/secchi_l0_a_seq_cor1_20120306_20120306_230000_s4c1a.fts
 input_filename = "../data/secchi_l0_a_seq_cor1_20120306_20120306_230000_s4c1a.fts"
 orig_img = fits.getdata(input_filename)
 print(orig_img.shape, orig_img.dtype)
 print(np.min(orig_img), np.mean(orig_img), np.max(orig_img))
-#plt.imshow(orig_img, cmap="gray")
+# plt.imshow(orig_img, cmap="gray")
 
 # 1 compress
 # device = 'cuda' if torch.cuda.is_available() else 'cpu'
-device = 'cpu'
+device = "cpu"
 
 # (default) quality=2 ~ 200x compression
 net = bmshj2018_factorized(quality=2, pretrained=True).eval().to(device)
-print(f'Parameters: {sum(p.numel() for p in net.parameters())}')
+print(f"Parameters: {sum(p.numel() for p in net.parameters())}")
 
 img = orig_img.copy()
 
-print("original data range (min,mean,max):", np.min(img), np.mean(img), np.max(img)) # 0-16k
+print(
+    "original data range (min,mean,max):", np.min(img), np.mean(img), np.max(img)
+)  # 0-16k
 
-img = np.asarray([img,img,img]) # fake rgb
+img = np.asarray([img, img, img])  # fake rgb
 img = np.transpose(img, (1, 2, 0)).astype(np.int16)
 
 # normalise to [0, 255]
-img = (img - img.min()) / (
-    img.max() - img.min()
-)
+img = (img - img.min()) / (img.max() - img.min())
 img = img.astype(np.float32)
-print("normalised data range (min,mean,max):", np.min(img), np.mean(img), np.max(img)) # 0-1
+print(
+    "normalised data range (min,mean,max):", np.min(img), np.mean(img), np.max(img)
+)  # 0-1
 
 x = transforms.ToTensor()(img)
 x = x.unsqueeze(0).to(device)
-print("x data range (min,mean,max):", torch.min(x), torch.mean(x), torch.max(x)) # 0-1
+print("x data range (min,mean,max):", torch.min(x), torch.mean(x), torch.max(x))  # 0-1
 
 with torch.no_grad():
     # Full pass: out_net = net.forward(x)
@@ -46,100 +53,117 @@ with torch.no_grad():
     y = net.g_a(x)
     print("y", y.shape)
     y_strings = net.entropy_bottleneck.compress(y)
-    print("len(y_strings) = ",len(y_strings[0]))
+    print("len(y_strings) = ", len(y_strings[0]))
 
     strings = [y_strings]
     shape = y.size()[-2:]
-    
+
 print("for comparison, this is what we have now:")
 print("compressed_strings", len(strings))
 print("compressed_strings[0][0]", len(strings[0][0]))
 
 print(type(strings[0][0]))
 print(shape)
-latent_name = "latent_" + str(shape[0])+"_"+str(shape[1])
+latent_name = "latent_" + str(shape[0]) + "_" + str(shape[1])
 
 # Save compressed forms:
-with open(latent_name+".bytes", 'wb') as f:
+with open(latent_name + ".bytes", "wb") as f:
     f.write(strings[0][0])
-    
+
 
 # 2 decompress
 
-with open(latent_name+".bytes", "rb") as f:
+with open(latent_name + ".bytes", "rb") as f:
     strings_loaded = f.read()
 strings_loaded = [[strings_loaded]]
 
 a, b = int(latent_name.split("_")[1]), int(latent_name.split("_")[2])
-shape_loaded = ([a,b])
+shape_loaded = [a, b]
 
 with torch.no_grad():
     out_net = net.decompress(strings_loaded, shape_loaded)
-    #(is already called inside) out_net['x_hat'].clamp_(0, 1)
+    # (is already called inside) out_net['x_hat'].clamp_(0, 1)
 
-x_hat = out_net['x_hat']
-print("x_hat data range (min,mean,max):", torch.min(x_hat), torch.mean(x_hat), torch.max(x_hat)) # 0-1
+x_hat = out_net["x_hat"]
+print(
+    "x_hat data range (min,mean,max):",
+    torch.min(x_hat),
+    torch.mean(x_hat),
+    torch.max(x_hat),
+)  # 0-1
 
 print(out_net.keys())
 
-rec_net = transforms.ToPILImage()(out_net['x_hat'].squeeze().cpu())
-print("reconstruction data range (min,mean,max):", np.min(rec_net), np.mean(rec_net), np.max(rec_net)) # 0-255 again
+rec_net = transforms.ToPILImage()(out_net["x_hat"].squeeze().cpu())
+print(
+    "reconstruction data range (min,mean,max):",
+    np.min(rec_net),
+    np.mean(rec_net),
+    np.max(rec_net),
+)  # 0-255 again
 
-diff = torch.mean((out_net['x_hat'] - x).abs(), axis=1).squeeze().cpu()
+diff = torch.mean((out_net["x_hat"] - x).abs(), axis=1).squeeze().cpu()
 
 fix, axes = plt.subplots(1, 3, figsize=(16, 12))
 for ax in axes:
-    ax.axis('off')
+    ax.axis("off")
 
 axes[0].imshow(img)
-axes[0].title.set_text('Original')
+axes[0].title.set_text("Original")
 
 axes[1].imshow(rec_net)
-axes[1].title.set_text('Reconstructed')
+axes[1].title.set_text("Reconstructed")
 
-im = axes[2].imshow(diff, cmap='viridis')
-axes[2].title.set_text('Difference')
+im = axes[2].imshow(diff, cmap="viridis")
+axes[2].title.set_text("Difference")
 plt.colorbar(im, ax=axes[2])
 
 plt.show()
 
 # 3 metrics
 
+
 def compute_psnr(a, b):
-    mse = torch.mean((a - b)**2).item()
+    mse = torch.mean((a - b) ** 2).item()
     return -10 * math.log10(mse)
 
+
 def compute_msssim(a, b):
-    return ms_ssim(a, b, data_range=1.).item()
+    return ms_ssim(a, b, data_range=1.0).item()
+
 
 def compute_bpp(out_net):
-    size = out_net['x_hat'].size()
+    size = out_net["x_hat"].size()
     num_pixels = size[0] * size[2] * size[3]
-    return sum(torch.log(likelihoods).sum() / (-math.log(2) * num_pixels)
-              for likelihoods in out_net['likelihoods'].values()).item()
+    return sum(
+        torch.log(likelihoods).sum() / (-math.log(2) * num_pixels)
+        for likelihoods in out_net["likelihoods"].values()
+    ).item()
+
 
 def convert_size(size_bytes):
-   if size_bytes == 0:
-       return "0B"
-   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-   i = int(math.floor(math.log(size_bytes, 1024)))
-   p = math.pow(1024, i)
-   s = round(size_bytes / p, 2)
-   return "%s %s" % (s, size_name[i])
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
+
 
 def files_size(file_path):
     size_bytes = os.path.getsize(file_path)
     print("File", file_path, "has", convert_size(size_bytes))
     return size_bytes
 
+
 print(f'PSNR: {compute_psnr(x, out_net["x_hat"]):.2f}dB')
 print(f'MS-SSIM: {compute_msssim(x, out_net["x_hat"]):.4f}')
-if 'likelihoods' in out_net.keys():
-    print(f'Bit-rate: {compute_bpp(out_net):.3f} bpp')
+if "likelihoods" in out_net.keys():
+    print(f"Bit-rate: {compute_bpp(out_net):.3f} bpp")
 
 original_size = files_size(input_filename)
-latent_size = files_size(latent_name+".bytes")
+latent_size = files_size(latent_name + ".bytes")
 
 reduction_factor = original_size / latent_size
-print("Compressed with reduction factor by", round(reduction_factor,2), "times")
-
+print("Compressed with reduction factor by", round(reduction_factor, 2), "times")
--- src/icarus/onboard/payload/01_compress_ai/openvino_use_model.py
+++ src/icarus/onboard/payload/01_compress_ai/openvino_use_model.py
@@ -1,29 +1,32 @@
 from pathlib import Path
+
 # Works with older 2021 version:
 import openvino
 import openvino.inference_engine
 from openvino.inference_engine import IECore
+
 ie = IECore()
 
-import sys
-import logging
-import numpy as np
 import glob
-from typing import Callable
+import logging
+import sys
 import time
+from typing import Callable
 
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
 from astropy.io import fits
 from torchvision import transforms
-import torch
 
 # 1 load openvino model
+
 
 class OpenVinoModel:
     def __init__(self, ie, model_path, batch_size=1):
         self.logger = logging.getLogger("model")
-        print('\tModel path: {}'.format(model_path))
-        self.net = ie.read_network(model_path, model_path[:-4] + '.bin')
+        print("\tModel path: {}".format(model_path))
+        self.net = ie.read_network(model_path, model_path[:-4] + ".bin")
         self.set_batch_size(batch_size)
 
     def preprocess(self, inputs):
@@ -44,24 +47,26 @@ class OpenVinoModel:
 def device_available():
     ie = IECore()
     devs = ie.available_devices
-    return 'MYRIAD' in devs
+    return "MYRIAD" in devs
 
 
-def load_model(model_path, device='MYRIAD') -> Callable:
+def load_model(model_path, device="MYRIAD") -> Callable:
     # Broadly copied from the OpenVINO Python examples
     ie = IECore()
     try:
-        ie.unregister_plugin('MYRIAD')
+        ie.unregister_plugin("MYRIAD")
     except:
         pass
 
     model = OpenVinoModel(ie, model_path)
     tic = time.time()
-    print('Loading ONNX network to ',device,'...')
+    print("Loading ONNX network to ", device, "...")
 
-    exec_net = ie.load_network(network=model.net, device_name=device,config=None, num_requests=1)
+    exec_net = ie.load_network(
+        network=model.net, device_name=device, config=None, num_requests=1
+    )
     toc = time.time()
-    print('one, time elapsed : {} seconds'.format(toc - tic))
+    print("one, time elapsed : {} seconds".format(toc - tic))
 
     def predict(x: np.ndarray) -> np.ndarray:
         """
@@ -76,73 +81,78 @@ def load_model(model_path, device='MYRIAD') -> Callable:
         """
         print(x.shape)
 
-        result = exec_net.infer({'input': x[np.newaxis]})
-        return result['output'][0]  # (n_class, H, W)
+        result = exec_net.infer({"input": x[np.newaxis]})
+        return result["output"][0]  # (n_class, H, W)
 
     return predict
+
 
 BATCH_SIZE = 1
 
 example_input = np.random.rand(BATCH_SIZE, 3, 512, 512)
 model_path = "onboard_net.onnx"
 
-device = 'CPU'
+device = "CPU"
 ie = IECore()
 model = OpenVinoModel(ie, model_path)
-exec_net = ie.load_network(network=model.net, device_name=device, config=None, num_requests=1)
+exec_net = ie.load_network(
+    network=model.net, device_name=device, config=None, num_requests=1
+)
 
-result = exec_net.infer({'input': example_input})
+result = exec_net.infer({"input": example_input})
 
 for output in list(exec_net.outputs.keys()):
-  print("output named",output,"gives", result[output].shape)
-  
-  
+    print("output named", output, "gives", result[output].shape)
+
+
 # 2 run with real data
 
 input_filename = "../data/secchi_l0_a_seq_cor1_20120306_20120306_230000_s4c1a.fts"
 orig_img = fits.getdata(input_filename)
 print(orig_img.shape, orig_img.dtype)
 print(np.min(orig_img), np.mean(orig_img), np.max(orig_img))
-#plt.imshow(orig_img, cmap="gray")
+# plt.imshow(orig_img, cmap="gray")
 
 img = orig_img.copy()
 
-print("original data range (min,mean,max):", np.min(img), np.mean(img), np.max(img)) # 0-16k
+print(
+    "original data range (min,mean,max):", np.min(img), np.mean(img), np.max(img)
+)  # 0-16k
 
-img = np.asarray([img,img,img]) # fake rgb
+img = np.asarray([img, img, img])  # fake rgb
 img = np.transpose(img, (1, 2, 0)).astype(np.int16)
 
 # normalise to [0, 255]
-img = (img - img.min()) / (
-    img.max() - img.min()
-)
+img = (img - img.min()) / (img.max() - img.min())
 img = img.astype(np.float32)
-print("normalised data range (min,mean,max):", np.min(img), np.mean(img), np.max(img)) # 0-1
+print(
+    "normalised data range (min,mean,max):", np.min(img), np.mean(img), np.max(img)
+)  # 0-1
 
 x = transforms.ToTensor()(img)
-x = x.unsqueeze(0).to('cpu')
-print("x data range (min,mean,max):", torch.min(x), torch.mean(x), torch.max(x)) # 0-1
+x = x.unsqueeze(0).to("cpu")
+print("x data range (min,mean,max):", torch.min(x), torch.mean(x), torch.max(x))  # 0-1
 
 x_np = x.cpu().detach().numpy()
 print(x_np.shape)
 
 ## Inference:
 
-result = exec_net.infer({'input': x_np})
+result = exec_net.infer({"input": x_np})
 
 for output in list(exec_net.outputs.keys()):
-  print("output named",output,"gives", result[output].shape)
-  
-decompressed = result['output']
+    print("output named", output, "gives", result[output].shape)
+
+decompressed = result["output"]
 print("decompressed.shape", decompressed.shape)
 
 decompressed_for_plot = np.transpose(decompressed[0], (1, 2, 0))
 
 fig, ax = plt.subplots(nrows=1, ncols=2)
 
-ax[0].title.set_text('Input')
+ax[0].title.set_text("Input")
 ax[0].imshow(orig_img, cmap="gray")
-ax[1].title.set_text('Decompressed from onxx model')
+ax[1].title.set_text("Decompressed from onxx model")
 ax[1].imshow(decompressed_for_plot, cmap="gray")
 plt.show()
 
@@ -154,12 +164,11 @@ print("Timing", num_images, "inference passes")
 
 start = time.perf_counter()
 for _ in range(num_images):
-    result = exec_net.infer({'input': x_np})
-    example_output = result['output']
+    result = exec_net.infer({"input": x_np})
+    example_output = result["output"]
 end = time.perf_counter()
 time_onnx = end - start
 print(
     f"ONNX model in OpenVINO CPU Runtime/CPU: {time_onnx/num_images:.3f} "
     f"seconds per image, FPS: {num_images/time_onnx:.2f}"
 )
-
