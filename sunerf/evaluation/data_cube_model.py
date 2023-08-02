ec30117f9f59bd19d61b38cea5d8b40b7013b4c0
Martin Sanner <martin.sanner@gmx.de>
2023-08-02 19:42:57 +0000

    Add Sun, Earth, L5 to plot, set alpha to be exponent in velocity

--- sunerf/evaluation/data_cube_model.py
+++ sunerf/evaluation/data_cube_model.py
@@ -12,8 +12,7 @@ import cv2
 
 from mpl_toolkits.mplot3d import Axes3D
 
-base_path = '/mnt/training/HAO_pinn_cr_2view_a26978f_heliographic_reformat'
-observer_offset = np.deg2rad(90)
+base_path = '/mnt/training/HAO_pinn_2view'#'/mnt/training/HAO_pinn_cr_2view_a26978f_heliographic_reformat'
 
 chk_path = os.path.join(base_path, 'save_state.snf')
 video_path_dens = os.path.join(base_path, 'video_cube')
@@ -24,13 +23,13 @@ os.makedirs(video_path_dens, exist_ok=True)
 n_cubes = 70 # How many cubes to generate
 
 # Points in R_solar
-x = np.linspace(-100,100,32)
-y = np.linspace(-100,100,32)
-z = np.linspace(-100,100,32)
+x = np.linspace(-250,250,32)
+y = np.linspace(-250,250,32)
+z = np.linspace(-250,250,32)
 
 xx,yy,zz = np.meshgrid(x,y,z,indexing = "ij")
-sphere_center = np.array([0,0,0])
-distance = np.sqrt((xx - sphere_center[0])**2 + (yy - sphere_center[1])**2 + (zz - sphere_center[2])**2)
+solar_center = np.array([0,0,0])
+distance = np.sqrt((xx - solar_center[0])**2 + (yy - solar_center[1])**2 + (zz - solar_center[2])**2)
 distance_mask = 21 #Cut out 21 solar radii as per rest of the program
 outside_sphere_mask = distance > distance_mask
 x_filtered = xx[outside_sphere_mask]
@@ -98,13 +97,57 @@ for i, (rho, v, abs_v) in enumerate(zip(densities, velocities, speeds)):
     # Plot velocity
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x_filtered, y_filtered, z_filtered, c=norm_mag, alpha = norm_mag, marker='.')
+    ax.scatter(x_filtered, y_filtered, z_filtered, c=norm_mag, cmap = "inferno", alpha = norm_mag**2, marker='.') # norm_mag exponentiated for alpha channel
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     ax.set_title('3D Scatter Plot of points based on speed')
+    # Add Sun
+    u = np.linspace(0, 2 * np.pi, 100)
+    v = np.linspace(0, np.pi, 100)
+    x_sun = distance_mask * np.outer(np.cos(u), np.sin(v))
+    y_sun = distance_mask * np.outer(np.sin(u), np.sin(v))
+    z_sun = distance_mask * np.outer(np.ones(np.size(u)), np.cos(v))
+
+    # Plot the Sun
+    ax.plot_surface(x_sun, y_sun, z_sun, color='gold', alpha=0.5)
+
+    # Add Earth
+    radius_earth = 0.009157683 # Solar radii = 6371 km - its a dot.
+    x_earth = np.outer(np.cos(u), np.sin(v))# * radius_earth
+    y_earth = np.outer(np.sin(u), np.sin(v))# * radius_earth
+    z_earth = np.outer(np.ones(np.size(u)), np.cos(v))# * radius_earth
+
+    # Earth position defined on x axis, 1 AU = 215.032 Solar Radii
+    r_earth = 215.032
+    center_earth = (-r_earth, 0, 0)
+    # If center_earth = (-r_earth, 0, 0) then on a circle, this is at pi radians. If L5 is trailing, then L5 is at (r_earth*cos(2/3*np.pi), r_earth*sin(2/3*np.pi), 0)
+    center_l5 = (r_earth*np.cos(2/3*np.pi), r_earth*np.sin(2/3*np.pi), 0)
+    x_earth += center_earth[0]
+    y_earth += center_earth[1]
+    z_earth += center_earth[2]
+
+    # Plot the Earth
+    ax.plot_surface(x_earth, y_earth, z_earth, color='lightblue', alpha=0.9)
+    # Plot L5
+    x_l5 = x_earth + center_l5[0]
+    y_l5 = x_earth + center_l5[1]
+    z_l5 = x_earth + center_l5[2]
+    ax.plot_surface(x_l5, y_l5, z_l5, color = "lightslategray", alpha = 0.9)
+
+
+    # Plot earth orbit
+    x_orbit = r_earth * np.cos(u)
+    y_orbit = r_earth * np.sin(u)
+    z_orbit = np.zeros_like(u)
+
+    ax.scatter(x_orbit, y_orbit, z_orbit, c='gray', marker='.', alpha = 0.3)
+
+
     cbar = plt.colorbar(ax.collections[0], ax=ax)
     cbar.set_label('|v|')
+
+
     #plt.show()
     fig.savefig(os.path.join(video_path_dens, f'velocity_cube_{i:03d}.jpg'), dpi=100)
     
