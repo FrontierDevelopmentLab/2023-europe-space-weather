0ae255ce4874797e579b36b660770afe7178ac1f
Martin Sanner <martin.sanner@gmx.de>
2023-08-03 09:28:50 +0000

    Fixed density

--- sunerf/evaluation/data_cube_model.py
+++ sunerf/evaluation/data_cube_model.py
@@ -52,8 +52,9 @@ for i, timei in tqdm(enumerate(pd.date_range(loader.start_time, loader.end_time,
     query_points = torch.from_numpy(query_points_npy)
     enc_query_points = loader.encoding_fn(query_points.view(-1, 4))
     raw = loader.fine_model(enc_query_points)
-    
-    density = 10 ** (15 + raw[..., 0])
+    #electron_density = 10 ** (15 + x[..., 0])
+    #velocity = torch.tanh(x[..., 1:]) / 3 * 250 + 50
+    density = raw[...,0] # Function has been moved into the model, either remove it from the model or not.
     velocity = raw[..., 1:]
 
     if torch.isnan(density).any() or torch.isnan(velocity).any() or torch.isinf(density).any() or torch.isinf(velocity).any():
@@ -83,10 +84,10 @@ density_filenames = []
 velocity_filenames = []
 
 for i, (rho, v, abs_v) in enumerate(zip(densities, velocities, speeds)):
-    #rho_norm = (rho - global_min_rho)/(global_max_rho - global_min_rho + 1e-20)
+    rho_norm = (rho - global_min_rho)/(global_max_rho - global_min_rho + 1e-20)
     norm_mag = (abs_v-global_min_v)/(global_max_v-global_min_v)
     plt.close("all")
-    '''
+    
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(x_filtered, y_filtered, z_filtered, c=rho, marker='.',norm='log', alpha = rho_norm, vmin=2e24, vmax=8e28)
@@ -99,10 +100,11 @@ for i, (rho, v, abs_v) in enumerate(zip(densities, velocities, speeds)):
     #plt.show()
     fig.savefig(os.path.join(video_path_dens, f'density_cube_{i:03d}.jpg'), dpi=100)
     density_filenames.append(os.path.join(video_path_dens, f'density_cube_{i:03d}.jpg'))
-    '''
+    
     # Plot velocity
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    # Only plot pixels above certain velocity/Density
     ax.scatter(x_filtered, y_filtered, z_filtered, c=norm_mag, cmap = "inferno", alpha = norm_mag**1.5, marker='.') # norm_mag exponentiated for alpha channel
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
@@ -116,6 +118,7 @@ for i, (rho, v, abs_v) in enumerate(zip(densities, velocities, speeds)):
     z_sun = distance_mask * np.outer(np.ones(np.size(u)), np.cos(v))
 
     # Plot the Sun
+    
     ax.plot_surface(x_sun, y_sun, z_sun, color='gold', alpha=0.5)
 
     # Add Earth
@@ -163,10 +166,10 @@ frame_duration = 0.5 #2fps
 if len(density_filenames):
     with imageio.get_writer(os.path.join(video_path_dens,'density.gif'), mode='I', duration=frame_duration) as writer:
         for filename in density_filenames:
-            image = imageio.imread(filename)
+            image = imageio.v3.imread(filename)
             writer.append_data(image)
 if len(velocity_filenames):
     with imageio.get_writer(os.path.join(video_path_dens,'velocity.gif'), mode='I', duration=frame_duration) as writer:
         for filename in velocity_filenames:
-            image = imageio.imread(filename)
+            image = imageio.v3.imread(filename)
             writer.append_data(image)
