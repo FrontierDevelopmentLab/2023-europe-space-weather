e712941d0de64ac23c0ac1fefe9b58ab9afec07a
RobertJaro <robert.jarolim@gmail.com>
2023-08-13 19:34:17 +0200

    update loading vtk, add r2 filter

--- sunerf/evaluation/load_density_cube.py
+++ sunerf/evaluation/load_density_cube.py
@@ -9,22 +9,22 @@ from tvtk.api import tvtk, write_data
 from sunerf.evaluation.loader import SuNeRFLoader
 from sunerf.utilities.data_loader import normalize_datetime
 
-base_path = '/mnt/training/HAO_pinn_cr_2view_a26978f_heliographic_reformat'
+base_path = '/mnt/training/HAO_pinn_cr_allview_a26978f_heliographic'
 chk_path = os.path.join(base_path, 'save_state.snf')
-save_path = os.path.join(base_path, 'vtk')
+save_path = os.path.join(base_path, 'vtk_128')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # init loader
-resolution = 256
+resolution = 128
 loader = SuNeRFLoader(chk_path)
 
-n_time_points = 10
+n_time_points = 100
 batch_size = 4096 * 4 * torch.cuda.device_count()
 os.makedirs(save_path, exist_ok=True)
 
 
-def save_vtk(vec, path, name, scalar=None, scalar_name='scalar', Mm_per_pix=1):
+def save_vtk(vec, path, name, scalar=None, scalar_name='scalar', sr_per_pix=1):
     """Save numpy array as VTK file
 
     :param vec: numpy array of the vector field (x, y, z, c)
@@ -35,7 +35,7 @@ def save_vtk(vec, path, name, scalar=None, scalar_name='scalar', Mm_per_pix=1):
     # Unpack
     dim = vec.shape[:-1]
     # Generate the grid
-    pts = np.stack(np.mgrid[0:dim[0], 0:dim[1], 0:dim[2]], -1).astype(np.int64) * Mm_per_pix
+    pts = np.stack(np.mgrid[0:dim[0], 0:dim[1], 0:dim[2]], -1).astype(np.int64) * sr_per_pix
     # reorder the points and vectors in agreement with VTK
     # requirement of x first, y next and z last.
     pts = pts.transpose(2, 1, 0, 3)
@@ -67,7 +67,11 @@ with torch.no_grad():
             np.linspace(-100, 100, resolution, dtype=np.float32),
             np.ones((1,), dtype=np.float32) * time, indexing='ij'), -1)
 
-        mask = np.sqrt(np.sum(query_points_npy[:, :, :, 0, :3] ** 2, axis=-1)) < 21
+        radius = np.sqrt(np.sum(query_points_npy[:, :, :, 0, :3] ** 2, axis=-1))
+        mask = (radius < 21) | (radius > 100)
+
+        r2_mask = radius ** 2 #((radius - 20) / (60 - 20)) ** 2
+        # r2_mask = np.clip(r2_mask, 0, 1)
 
         query_points = torch.from_numpy(query_points_npy)
 
@@ -89,6 +93,9 @@ with torch.no_grad():
         density = density.view(query_points_npy.shape[:3]).cpu().detach().numpy()
         velocity = velocity.view(query_points_npy.shape[:3] + velocity.shape[-1:]).cpu().detach().numpy()
         #
+        # scale density
+        density *= r2_mask
+        #
         velocity = velocity  # * density[..., None] / 1e27 # scale to mass flux
         # apply mask
         density[mask] = 0  # np.nan
@@ -96,4 +103,4 @@ with torch.no_grad():
 
         print('save vtk')
         vtk_filename = os.path.join(save_path, f"data_cube_{timei.isoformat('T', timespec='minutes')}.vtk")
-        save_vtk(velocity, vtk_filename, "v", density, "density")
+        save_vtk(velocity, vtk_filename, "v", density, "density", sr_per_pix= 200 / resolution)
