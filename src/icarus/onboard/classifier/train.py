76767678ed80744001dc0b0bdcf07ee95f6a5868
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2023-08-17 10:13:19 +0000

    improved logging + config

--- src/icarus/onboard/classifier/train.py
+++ src/icarus/onboard/classifier/train.py
@@ -17,7 +17,11 @@ from PIL import Image
 from rich.progress import track
 from torch import nn
 from torch.utils.data import DataLoader, Dataset
-from torchmetrics import Accuracy
+from torchmetrics.classification import (
+    BinaryAccuracy,
+    BinaryF1Score,
+    BinaryConfusionMatrix,
+)
 
 torch.multiprocessing.set_sharing_strategy("file_system")
 import json
@@ -61,9 +65,9 @@ class CME_classifier(LightningModule):
         # define model
         if "resnet" in self.hparams.model:
             if self.hparams.model == "resnet18":
-                self.model = models.resnet18(pretrained=pretrained)
+                self.model = models.resnet18(weights=pretrained)
             elif self.hparams.model == "resnet50":
-                self.model = models.resnet50(pretrained=pretrained)
+                self.model = models.resnet50(weights=pretrained)
 
             linear_size = list(self.model.children())[-1].in_features
             self.model.fc = nn.Linear(linear_size, 1)
@@ -80,7 +84,9 @@ class CME_classifier(LightningModule):
         else:
             print(self.hparams.loss, "loss not implemented")
 
-        self.metric = Accuracy(task="binary")
+        self.accuracy = BinaryAccuracy()
+        self.f1 = BinaryF1Score()
+        self.confusion = BinaryConfusionMatrix()
 
         self.batch_size = batch_size
 
@@ -89,23 +95,21 @@ class CME_classifier(LightningModule):
 
     def training_step(self, batch, batch_idx):
         images, y_target = batch
-        images = (
-            images.unsqueeze(1).repeat(1, 3, 1, 1).float()
-        )
+        images = images.repeat(1, 3, 1, 1).float()
         x = images
         y = self.forward(x).flatten()
 
         y_target = y_target.float()
         loss = self.loss(y, y_target)
 
-        # if batch_idx % 10 == 0:
-        self.log(
-            "train/loss",
-            loss,
-            prog_bar=True,
-            sync_dist=True,
-            batch_size=self.batch_size,
-        )
+        if batch_idx % 10 == 0 and wandb.run:
+            self.log(
+                "train/loss",
+                loss,
+                prog_bar=True,
+                sync_dist=True,
+                batch_size=self.batch_size,
+            )
 
         return loss
 
@@ -115,21 +119,25 @@ class CME_classifier(LightningModule):
     def validation_step(self, batch, batch_idx):
         self.model.eval()
         images, y_target = batch
-        images = (
-            images.unsqueeze(1).repeat(1, 3, 1, 1).float()
-        )
+        images = images.repeat(1, 3, 1, 1).float()
         x = images
         y = self.forward(x).flatten()
 
         y_target = y_target.float()
         loss = self.loss(y, y_target)
-        self.metric.update(y, y_target)
+        self.f1.update(y, y_target)
+        self.accuracy.update(y, y_target)
+        self.confusion.update(y, y_target)
 
-        # if batch_idx % 10 == 0:
-        self.log(
-            "val/loss", loss, prog_bar=True, sync_dist=True, batch_size=self.batch_size
-        )
-        if batch_idx == self.validation_batch_index:
+        if batch_idx % 10 == 0 and wandb.run:
+            self.log(
+                "val/loss",
+                loss,
+                prog_bar=True,
+                sync_dist=True,
+                batch_size=self.batch_size,
+            )
+        if batch_idx == self.validation_batch_index and wandb.run:
             x_vis = (x[0, 0] - x[0, 0].min()) / (x[0, 0].max() - x[0, 0].min())
             table = wandb.Table(
                 columns=["Observation_image", "Predicted_label", "Ground truth label"]
@@ -143,9 +151,7 @@ class CME_classifier(LightningModule):
     def test_step(self, batch, batch_idx):
         self.model.eval()
         images, y_target = batch
-        images = (
-            images.unsqueeze(1).repeat(1, 3, 1, 1).float()
-        )
+        images = images.repeat(1, 3, 1, 1).float()
 
         x = images
         y = self.forward(x).flatten()
@@ -153,14 +159,34 @@ class CME_classifier(LightningModule):
         y_target = y_target.float()
         loss = self.loss(y, y_target)
 
-        # if batch_idx % 10 == 0:
-        self.log(
-            "test/loss", loss, prog_bar=True, sync_dist=True, batch_size=self.batch_size
-        )
+        if batch_idx % 10 == 0 and wandb.run:
+            self.log(
+                "test/loss",
+                loss,
+                prog_bar=True,
+                sync_dist=True,
+                batch_size=self.batch_size,
+            )
 
     def on_validation_epoch_end(self):
-        metric = self.metric.compute()
-        self.log("Accuracy:", metric, sync_dist=True, batch_size=self.batch_size)
+        accuracy = self.accuracy.compute()
+        f1 = self.f1.compute()
+        cm = self.confusion.compute()
+
+        self.log("val/accuracy", accuracy, sync_dist=True, batch_size=self.batch_size)
+        self.log("val/f1", f1, sync_dist=True, batch_size=self.batch_size)
+
+        tp = cm[1, 1].float()
+        fp = cm[0, 1].float()
+        tn = cm[0, 0].float()
+        fn = cm[1, 0].float()
+
+        tot = tp+fp+tn+fn
+
+        self.log("val/tp", tp/tot, sync_dist=True, batch_size=self.batch_size)
+        self.log("val/fp", fp/tot, sync_dist=True, batch_size=self.batch_size)
+        self.log("val/tn", tn/tot, sync_dist=True, batch_size=self.batch_size)
+        self.log("val/fn", fn/tot, sync_dist=True, batch_size=self.batch_size)
 
     def configure_optimizers(self):
         from torch.optim.lr_scheduler import ReduceLROnPlateau
@@ -224,16 +250,13 @@ if __name__ == "__main__":
     config_path = os.path.join(PROJECT_DIR, "config")
     config = get_config(config_path)
 
-    wandb.init(
+    wandb_logger = WandbLogger(
         project="CME_classification",
         entity="ssa_live_twin",
-        config=config,
+        name=config["train"]["run_id"],
         mode=config["train"]["wandb_mode"],
-        name=config["train"][
-            "run_id"
-        ],  # ToDo: This doesn't work for more than 1 batch size
+        config=config,
     )
-    wandb_logger = WandbLogger()
 
     # save best and last model
     checkpoint_callback = ModelCheckpoint(
@@ -244,7 +267,7 @@ if __name__ == "__main__":
         save_top_k=1,
         save_last=True,
         verbose=True,
-        every_n_train_steps=wandb.config.train["log_ckpt_every_n_steps"],
+        every_n_epochs=wandb.config.train["log_ckpt_every_n_epochs"],
     )
 
     # save_callback = LambdaCallback(on_validation_end=lambda *args: save_state(sunerf, data_module, save_path))
@@ -285,8 +308,7 @@ if __name__ == "__main__":
         accelerator="auto",
         callbacks=[lr_monitor, checkpoint_callback],
         logger=[csv_logger, wandb_logger],
-        # devices=1,
-        strategy=DDPStrategy(find_unused_parameters=True),
+        strategy=DDPStrategy(find_unused_parameters=False),
         log_every_n_steps=wandb.config.train["log_every_n_steps"],
     )
 
@@ -300,5 +322,3 @@ if __name__ == "__main__":
     # out_dir = config["train"]["out_dir"]
 
     wandb.finish()
-
-
